ba88583af0527419d6db24ea65caa291379e56fc
tanuu7 <104612953+tanuu7@users.noreply.github.com>
2022-07-02 15:55:01 +0530

    Update employee_info.c

--- employee_info.c
+++ employee_info.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-struct student
-{
-int rollno;
-char name[60];
-}s1;  //declaring s1 variable for structure
-void main( )
-{
-//store first employee information
-s1.rollno=1;
-strcpy(s1.name, “intellipaat”);//copying string into char array
-//printing first employee information
-printf( "Rollno : %d\n", s1.rollno);
-printf( "Name : %s\n", s1.name);
+
+// create struct with person1 variable
+struct Person {
+  char name[50];
+  int rollno;
+  float salary;
+} person1;
+
+int main() {
+
+  // assign value to name of person1
+  strcpy(person1.name, "Tanu chauhan");
+
+  // assign values to other person1 variables
+  person1.rollno = 1984;
+  person1. salary = 2500;
+
+  // print struct variables
+  printf("Name: %s\n", person1.name);
+  printf("Roll no.: %d\n", person1.rollno);
+  printf("Salary: %.2f", person1.salary);
+
+  return 0;
 }
